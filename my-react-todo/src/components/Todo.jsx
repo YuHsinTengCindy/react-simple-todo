8b4e7155548d6ed4eabfdf5f62f2733a6f62fcb1
YuHsinTengCindy <yuhsinteng@gmail.com>
2025-02-19 15:44:12 +0800

    react-simple-todo

---
+++ my-react-todo/src/components/Todo.jsx
@@ -0,0 +1,19 @@
+import { FaDeleteLeft } from "react-icons/fa6";
+import {  MdEdit } from "react-icons/md";
+import EditForm from "./Editfom";
+
+function Todo({todo, deleteTodo, toggleCompleted, toggleIsEditing,editTodo }){
+    return(
+    todo.isEditing ? <EditForm todo={todo} editTodo={editTodo}/>
+    :<div className={`todo ${todo.isCompleted ? 'completed ' : ''}`}>
+        <p onClick={()=> {toggleCompleted(todo.id)}}>{todo.content}</p>
+        <div>   
+        <MdEdit   onClick={()=>{toggleIsEditing(todo.id)}}
+        style={{cursor:'pointer'}}/>
+        <FaDeleteLeft  onClick={()=>{deleteTodo(todo.id)}} 
+        style={{cursor:'pointer', marginLeft:'5px'}}/>
+        </div>
+    </div>   
+    ) ;   
+}
+export default Todo;
